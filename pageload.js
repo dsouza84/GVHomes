3b17ad861423639f1b0ee8f8286184e6454b1b43
dsouz <thedansouza@gmail.com>
2018-08-15 16:07:48 -0700

    Updates 8-15-18

--- pageload.js
+++ pageload.js
@@ -1,6 +1,16 @@
 jQuery.noConflict();
-$(document).ready(function(){
-    $("#defaultcontent").click(function(){
-        $("#customer-care-content").load("defaultcontent.txt");
-    });
+jQuery(document).ready(function(){
+  jQuery("#defaultcontent").on('click touchstart', function(){
+    jQuery("#customer-care-content").load("https://s3-us-west-1.amazonaws.com/gvhomes/files/customer_care/defaultcontent.txt");
+  });
+});
+jQuery(document).ready(function(){
+  jQuery("#caring").on('click touchstart', function(){
+    jQuery("#customer-care-content").load("https://s3-us-west-1.amazonaws.com/gvhomes/files/customer_care/caringforyourhome.txt");
+  });
+});
+jQuery(document).ready(function(){
+  jQuery("#howto").on('click touchstart', function(){
+    jQuery("#customer-care-content").load("https://s3-us-west-1.amazonaws.com/gvhomes/files/customer_care/howto.txt");
+  });
 });
